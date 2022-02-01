a15764302e230e22613059cce9ef390412ad3707
Oleksandr Pavlishchev <velogitara@gmail.com>
2022-02-01 16:08:33 +0200

    works

--- src/js/readmore.js
+++ src/js/readmore.js
@@ -40,43 +40,43 @@ $('.products__button--green').click(function() {
 });
 
 
-var halfText = $('.products__text--pink--desc').innerHeight() / 2,
-    textHeight = $('.products__text--pink--desc').innerHeight();
+// var halfText = $('.products__text--pink--desc').innerHeight() / 2,
+//     textHeight = $('.products__text--pink--desc').innerHeight();
 
-$('.products__text--pink--desc').css('height', $('.products__text--pink--desc').innerHeight() / 2);
+// $('.products__text--pink--desc').css('height', $('.products__text--pink--desc').innerHeight() / 2);
 
-$('.products__button--pink--desc').click(function() {
-    if ($('.products__text--pink--desc').innerHeight() == halfText) {
-        $('.products__text--pink--desc').animate({ height: textHeight }, 500);
-    } else {
-        $('.products__text--pink--desc').animate({ height: halfText }, 500);
-    }
-});
+// $('.products__button--pink--desc').click(function() {
+//     if ($('.products__text--pink--desc').innerHeight() == halfText) {
+//         $('.products__text--pink--desc').animate({ height: textHeight }, 500);
+//     } else {
+//         $('.products__text--pink--desc').animate({ height: halfText }, 500);
+//     }
+// });
 
-var halfText = $('.products__text--yellow--desc').innerHeight() / 2,
-    textHeight = $('.products__text--yellow--desc').innerHeight();
+// var halfText = $('.products__text--yellow--desc').innerHeight() / 2,
+//     textHeight = $('.products__text--yellow--desc').innerHeight();
 
-$('.products__text--yellow--desc').css('height', $('.products__text--yellow').innerHeight() / 2);
+// $('.products__text--yellow--desc').css('height', $('.products__text--yellow').innerHeight() / 2);
 
-$('.products__button--yellow--desc').click(function() {
-    if ($('.products__text--yellow--desc').innerHeight() == halfText) {
-        $('.products__text--yellow--desc').animate({ height: textHeight }, 500);
-    } else {
-        $('.products__text--yellow--desc').animate({ height: halfText }, 500);
-    }
-});
+// $('.products__button--yellow--desc').click(function() {
+//     if ($('.products__text--yellow--desc').innerHeight() == halfText) {
+//         $('.products__text--yellow--desc').animate({ height: textHeight }, 500);
+//     } else {
+//         $('.products__text--yellow--desc').animate({ height: halfText }, 500);
+//     }
+// });
 
 
 
-var halfText = $('.products__text--green--desc').innerHeight() / 2,
-    textHeight = $('.products__text--green--desc').innerHeight();
+// var halfText = $('.products__text--green--desc').innerHeight() / 2,
+//     textHeight = $('.products__text--green--desc').innerHeight();
 
-$('.products__text--green--desc').css('height', $('.products__text--green--desc').innerHeight() / 2);
+// $('.products__text--green--desc').css('height', $('.products__text--green--desc').innerHeight() / 2);
 
-$('.products__button--green').click(function() {
-    if ($('.products__text--green--desc').innerHeight() == halfText) {
-        $('.products__text--green--desc').animate({ height: textHeight }, 500);
-    } else {
-        $('.products__text--green--desc').animate({ height: halfText }, 500);
-    }
-});
+// $('.products__button--green').click(function() {
+//     if ($('.products__text--green--desc').innerHeight() == halfText) {
+//         $('.products__text--green--desc').animate({ height: textHeight }, 500);
+//     } else {
+//         $('.products__text--green--desc').animate({ height: halfText }, 500);
+//     }
+// });
