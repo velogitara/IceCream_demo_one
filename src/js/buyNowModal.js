f9e365a8b2a8b39a71c45b1f6ebc57852d12f0bb
AnastasiiaZhyltsova <anastasiiazhyltsova@gmail.com>
2022-02-02 17:04:25 +0200

    modal js

--- src/js/buyNowModal.js
+++ src/js/buyNowModal.js
@@ -1,11 +1,13 @@
 (() => {
   const refs = {
-    openModalBtn: document.querySelector('[data-buy-open]'),
+      openModalBtn: document.querySelector('[data-buy-open]'),
+       openBtn: document.querySelector('[data-now-open]'),
     closeModalBtn: document.querySelector('[data-buy-close]'),
     modal: document.querySelector('[data-buy]'),
   };
 
-  refs.openModalBtn.addEventListener('click', toggleModal);
+    refs.openModalBtn.addEventListener('click', toggleModal);
+     refs.openBtn.addEventListener('click', toggleModal);
   refs.closeModalBtn.addEventListener('click', toggleModal);
 
   function toggleModal() {
